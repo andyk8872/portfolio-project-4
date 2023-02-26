657abef06a953d96ce13e198c6f5aa696b78332c
andyk8872 <andrewkennedy35@yahoo.ie>
2023-02-26 01:27:01 +0000

    settings syntax

--- team_building/settings.py
+++ team_building/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["team-building.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['team-building.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',    
+    'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
