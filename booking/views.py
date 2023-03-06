bdbcd42f552ae71c68c88b9ed9a4f88e25e4b1d2
andyk8872 <andrewkennedy35@yahoo.ie>
2023-03-06 07:36:19 +0000

    add a confirmation modal

--- booking/views.py
+++ booking/views.py
@@ -12,9 +12,6 @@ class HomePage(TemplateView):
     template_name = "index.html"
 
 
-# class MakeBooking(TemplateView):
-#     template_name = "make_booking.html"
-
 
 @login_required
 def make_booking(request):
@@ -32,10 +29,7 @@ def make_booking(request):
 
 @login_required
 def view_booking(request):
-    """
-    Lets staff member view all bookings, and
-    regular user only his own bookings.
-    """
+  
     if request.user.is_staff:
         bookings = Booking.objects.all()
     else:
@@ -46,35 +40,11 @@ def view_booking(request):
     return render(request, 'my_account.html', context)   
 
 
-# @login_required
-# def delete_booking(request, booking_id):
-#     """
-#     Deletes booking and sends an email to the user who placed it.
-#     John at tutor support helped med figure out this one.
-#     """
-#     if request.user.is_staff:
-#         try:
-#             booking = get_object_or_404(PlaceBooking, id=booking_id)
-          
-#             booking.delete()
-#             messages.success(request, 'Booking deleted successfully.')
-#             return redirect('my_account')
-#         except Http404 as err:
-#             messages.error(request, 'Oops, booking not found.')
-#             return redirect('my_account')
-#     else:
-#         return redirect('home')
-
-
 @login_required
 def delete_booking(request, booking_id):
     item = get_object_or_404(Booking, id=booking_id)
     item.delete()
     return redirect('home')     
-
-
-# class EditBooking(TemplateView):
-#     template_name = "edit_booking.html"
 
 
 @login_required
